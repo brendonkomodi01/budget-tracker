0746c8072fae6fb3ec10723f236281444ed8c6da
brendonkomodi01 <brendonkomodi01@gmail.com>
2026-04-28 11:50:00 +0200

    fix cors config

--- src/main/java/hu/progmasters/angularspringexamretakebudget/config/SpringWebConfig.java
+++ src/main/java/hu/progmasters/angularspringexamretakebudget/config/SpringWebConfig.java
@@ -20,7 +20,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
+
 @Configuration
 @ComponentScan({"hu.progmasters"})
 public class SpringWebConfig implements WebMvcConfigurer {
